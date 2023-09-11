cadc3b3aa613467e8e993a36cf17fa705941cea4
John David Dela Cruz <johndaviddc1@gmail.com>
2023-09-11 21:41:20 +0800

    Employee Manager

---
+++ src/main/java/dave/dev/employeemanager/repository/EmployeeRepo.java
@@ -0,0 +1,13 @@
+package dave.dev.employeemanager.repository;
+
+import dave.dev.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
